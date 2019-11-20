7f100e6ac2f5e46fa8880e718b74ec4134d6827f
asmpate1 <asmpate1@in.ibm.com>
2019-11-20 09:17:57 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-
+import { Note } from './note';
+import { NotesService } from './notes.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  errMessage: string;
-
+  errMessage: "Title and Text both are required fields";
+  note: Note = new Note();
+  notes: Array<Note> = [];
+  title: string;
+  text: string;
+  constructor(private notelist: NotesService) {
+  }
+  addNote1() {
+    if (this.note.text === '' || this.note.title === '') {
+      this.errMessage = "Title and Text both are required fields";
+    }
+    console.log("In addNote()");
+    this.notes.push(this.note);
+    this.notelist.addNote(this.note).subscribe((data) => {
+    },
+      error => {
+        this.errMessage = error.message;
+        console.log(this.errMessage);
+        this.notes.pop();
+      });
+    // console.log(this.note.text);
+  }
+  ngOnInit() {
+    this.note = new Note();
+    this.notelist.getNotes().subscribe(
+      data => {
+        console.log(data);
+        this.notes = data;
+      },
+      error => {
+        this.errMessage = error.message;
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,45 @@
 import { NgModule } from '@angular/core';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HeaderComponent} from './header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { NotesService } from './notes.service';
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+ declarations: [ HeaderComponent,AppComponent],
+ imports: [ MatToolbarModule,
+   MatExpansionModule,
+   BrowserModule,
+   BrowserAnimationsModule,
+   MatFormFieldModule,
+   FormsModule,
+   ReactiveFormsModule,
+   MatButtonModule,
+   MatButtonToggleModule,
+   MatCardModule,
+   MatRadioModule,
+   MatCheckboxModule,
+   MatIconModule,
+   MatInputModule,
+   MatSelectModule,
+   HttpClientModule,
+   HttpModule
+ ],
+ providers: [ NotesService],
+ bootstrap: [AppComponent ]
 })
 export class AppModule { }
